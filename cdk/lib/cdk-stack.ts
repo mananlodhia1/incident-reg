f2eeabccd1e288a1efc183c33054fee2074f8813
mananlodhia1 <mananlodhia@hotmail.com>
2025-02-15 14:13:49 +1100

    update

--- cdk/lib/cdk-stack.ts
+++ cdk/lib/cdk-stack.ts
@@ -4,7 +4,6 @@ import * as ec2 from "aws-cdk-lib/aws-ec2";
 import * as iam from "aws-cdk-lib/aws-iam";
 import * as s3deployment from "aws-cdk-lib/aws-s3-deployment";
 import * as s3 from "aws-cdk-lib/aws-s3";
-import * as path from 'path';
 
 export class CdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -77,7 +76,7 @@ export class CdkStack extends cdk.Stack {
     new s3deployment.BucketDeployment(this, "DeployApp", {
       sources: [
         s3deployment.Source.asset(
-          path.join(__dirname, '..', 'backend.zip')
+          "backend.zip"
         ),
       ],
       destinationBucket: s3Bucket,
